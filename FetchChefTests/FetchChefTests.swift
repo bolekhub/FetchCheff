0813854b1d88f165f11a9f0b46a60fdd6d7e36ac
Boris Chirino f <boris@abielcode.com>
2025-01-13 11:01:09 -0500

    Initial Commit

---
+++ FetchChefTests/FetchChefTests.swift
@@ -0,0 +1,17 @@
+//
+//  FetchChefTests.swift
+//  FetchChefTests
+//
+//  Created by Boris Chirino Fernández on 1/13/25.
+//
+
+import Testing
+@testable import FetchChef
+
+struct FetchChefTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
